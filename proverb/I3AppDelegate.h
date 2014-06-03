7077eddff6e7ce878ce2ae2cb4bbc6d508a80e69
nekomoaruku <neko.mo.aruku@gmail.com>
2014-06-03 21:59:28 +0900

    Initial Commit

---
+++ proverb/I3AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  I3AppDelegate.h
+//  proverb
+//
+//  Created by Manabu Iwata on 6/3/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface I3AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ proverb/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  proverb
+//
+//  Created by Manabu Iwata on 6/3/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "I3AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([I3AppDelegate class]));
+    }
+}
